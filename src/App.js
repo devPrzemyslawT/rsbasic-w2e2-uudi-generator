6bbf465ef0212152b1a54bdb3eb9e852cdb3d60c
devPrzemyslawT <przemyslaw.tomaszewski.dev@gmail.com>
2023-02-22 15:28:50 +0100

    install uuidv library and code generating

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const { v4: uuidv4 } = require("uuid");
+	let value = uuidv4(); // ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
+
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<img src={logo} className='App-logo' alt='logo' />
+				<h2>UUID generator</h2>
+				<p>New code = {value}</p>
+			</header>
+		</div>
+	);
 }
 
 export default App;
